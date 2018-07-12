3e836346063d6ca732ce336a31fb5458a701f1d6
Justine Bricknell <justinegayton@gmail.com>
2018-07-12 18:32:48 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,19 +7,20 @@
 #create an instance method - parse
 # an INSTANCE METHOD calls on a SPECIFIC INSTANCE of that class! not the enitre class...
 #delete duplicate emails (detect, sortby?...)
+#for parse -- remember, you cannot iterate through string - need to do so through an array!
+#  @emails = [] You don't need this in your class body bc we are setting an array in the parse instance meth and we don't need to save an array for the class (for these specs)
+
 
 require 'pry'
 
 class EmailParser
   attr_accessor :emails
-#  @emails = []
 
   def initialize(emails)
     @emails = emails
   end
 
   def parse
-    #cannot iterate through string - need to do so through an array!
     parsed_emails = []
     parsed_emails = @emails.split(/[,\s]+/).uniq
   end
